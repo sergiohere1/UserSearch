3bc538b36a71aacb9ce771416b93572b168a715b
sergiohere1 <sergio11garcia@hotmail.com>
2017-11-06 09:21:21 +0100

    Add project files.

---
+++ UserSearch/UserSearch/Contacto.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LecturaFicheros
+{
+    public class Contacto : IEnumerable
+    {
+        /// <summary>
+        /// Campos pertenecientes a Contacto.
+        /// </summary>
+        protected String nombre;
+        protected int edad;
+        protected String nif;
+
+        /// <summary>
+        /// Constructor del Contacto, el cual recibe nombre, edad y NIF.
+        /// </summary>
+        /// <param name="nombre"></param>
+        /// <param name="edad"></param>
+        /// <param name="NIF"></param>
+        public Contacto(String nombre, int edad, String nif)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.nif = nif;
+        }
+
+        public String Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Edad
+        {
+            get
+            {
+                return edad.ToString();
+            }
+            set
+            {
+                edad = int.Parse(value);
+            }
+        }
+
+        public String Nif
+        {
+            get
+            {
+                return nif;
+            }
+            set
+            {
+                nif = value;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// Tostring en el cual mostramos los datos del contacto.
+        /// </summary>
+        /// <returns>Descripción del Contacto</returns>
+        override
+        public string ToString()
+        {
+            return string.Format("{0} \n {1} \n {2}", nombre, edad, nif);
+        }
+    }
+}
---
+++ UserSearch/UserSearch/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using LecturaFicheros;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UserSearch
+{
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<Contacto> contactos = new ObservableCollection<Contacto>();
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            readandInsertContacts();
+            idListaContacto.ItemsSource = contactos;           
+        }
+
+        public void readandInsertContacts()
+        {
+            int counter = 0;
+            string line;
+            Contacto contacto;
+            string nombre = "";
+            int edad = 0;
+            string nif = "";
+            string ruta = "UserSearch.Assets.Info.txt";
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream(ruta);
+
+
+            //Leer el fichero y mostrarlo línea a línea.
+            System.IO.StreamReader fichero = new System.IO.StreamReader(stream);
+
+            /*Bucle encargado de leer el fichero, ir almacenando los valores correspondientes
+             a nombre, edad y nif e ir almacenándolos en un nuevo contacto.*/
+            while ((line = fichero.ReadLine()) != null)
+            {
+                counter++;
+
+                switch (counter)
+                {
+                    case 1:
+                        nombre = line;
+                        break;
+                    case 2:
+                        int.TryParse(line, out edad);
+                        break;
+                    case 3:
+                        nif = line;
+                        contacto = new Contacto(nombre, edad, nif);
+                        contactos.Add(contacto);
+                        counter = 0;
+                        break;
+                }
+            }
+        }
+
+    }
+}
